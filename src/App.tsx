21c6757d8e2cae8a59cb1524a584ecad92a5a141
VladimirSolovyov <vovanchik3007@mail.ru>
2024-06-19 11:40:08 +0300

    Попытки поправить

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState, useCallback } from 'react'
 import './App.css'
 import Weather from './components/Weather'
 
+type WeatherVariable =
+	| 'weathercode'
+	| 'temperature_2m_max'
+	| 'temperature_2m_min'
+	| 'apparent_temperature_max'
+	| 'apparent_temperature_min'
+	| 'sunrise'
+	| 'sunset'
+	| 'precipitation_sum'
+	| 'rain_sum'
+	| 'showers_sum'
+	| 'snowfall_sum'
+	| 'precipitation_hours'
+	| 'windspeed_10m_max'
+	| 'windgusts_10m_max'
+	| 'inddirection_10m_dominant'
+	| 'shortwave_radiation_sum'
+	| 'et0_fao_evapotranspiration'
+	| ''
+
 function App() {
-	const [variables, setVariables] = useState<string[]>([
+	const [variables, setVariables] = useState<WeatherVariable[]>([
 		'rain_sum',
 		'snowfall_sum',
 	])
-	const [inputValue, setInputValue] = useState<string>('')
+	const [inputValue, setInputValue] = useState<WeatherVariable>('')
 
 	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setInputValue(e.target.value)
+		debugger
+		setInputValue(e.target.value as WeatherVariable)
 	}
 
 	const handleAddVariable = () => {
@@ -25,8 +46,7 @@ function App() {
 			<div>
 				<label>
 					{/* available values:
-                weathercode, temperature_2m_max, temperature_2m_min, apparent_temperature_max, apparent_temperature_min, sunrise, sunset, precipitation_sum, rain_sum,
-                showers_sum, snowfall_sum, precipitation_hours, windspeed_10m_max, windgusts_10m_max, winddirection_10m_dominant, shortwave_radiation_sum, et0_fao_evapotranspiration
+                
                 */}
 
 					<input
@@ -39,8 +59,53 @@ function App() {
 				</label>
 			</div>
 			<Weather lat={55.751244} long={37.618423} variables={variables} />
+			<Counter />
+			<ParentComponent />
 		</div>
 	)
 }
 
+function Counter() {
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		setCount(count + 1)
+		console.log(count) //0
+		setCount(count + 1)
+		console.log(count) //0
+	}, [])
+	/*count = 1 */
+	return <div>Count: {count}</div>
+}
+
+function ParentComponent() {
+	const [count, setCount] = useState(0)
+	const increment = useCallback(() => {
+		setCount(prev => prev + 1)
+	}, [])
+	return (
+		<div>
+			<button onClick={increment}>Increment</button>
+			<ChildComponent count={count} />
+		</div>
+	)
+}
+
+interface Count {
+	count: number
+}
+
+const ChildComponent: React.FC<Count> = React.memo(({ count }) => {
+	console.log('ChildComponent render')
+	return <div>Count: {count}</div>
+})
+
 export default App
+
+const obj: any = {
+	valueOf: function () {
+		return 30 // Возвращаемое значение, когда объект используется как число
+	},
+}
+
+console.log(obj * 2) //60
